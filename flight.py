c9ed24deccffd81278b8210f8fb71f613b55faa9
Ryansolar14 <76922192+Ryansolar14@users.noreply.github.com>
2022-10-21 13:00:37 -0400

    Fix Issues
    Fix a typo and change function scope to global.

--- flight.py
+++ flight.py
@@ -6,20 +6,35 @@
 # Credits: @shannon-s, ryzerobotics.com
 #
 
+#Import Libraries
+
 import sys
 import time
 import threading
 import socket
 
+#Global Functions
+
+def cmd(command):
+    sock.sendto(command.encode(encoding="utf-8"), tello_address)
+    print('Command sent: ' + command)
+
+def getData(command, type, symbol):
+    return_value = sock.sendto(command.encode(encoding="utf-8"), tello_address)
+    print('Command sent: ' + command)
+    print(type + ' is currently:' + str(return_value) + symbol)
+
+#Global Variables
 host = ''
 port = 9000
 locaddr = (host,port)
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-tello_address = ('192.168.10.1', 8889)
+tello_address = ('192.168.10.1', 8889) #Tello IP address and port, should not change
 sock.bind(locaddr)
 
+# Create and start receiving thread
 def recv():
     count = 0
     while True:
@@ -33,20 +48,13 @@ def recv():
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
+#Main Program
 try:
-    cmd('command')
-    getData('battery?', 'Battery Level', '%')
+    cmd('command') #Enter command mode
+    getData('battery?', 'Battery Level', '%') #Get battery level
 
-except KeyboradInterrupt:
+except KeyboardInterrupt:
     print('\n..\n')
     sock.close()
 
 
-def cmd(command):
-    sock.sendto(command.encode(encoding="utf-8"), tello_address)
-    print('Command sent: ' + command)
-
-def getData(command, type, symbol):
-    return_value = sock.sendto(command.encode(encoding="utf-8"), tello_address)
-    print('Command sent: ' + command)
-    print(type + ' is currently:' + str(return_value) + symbol)
